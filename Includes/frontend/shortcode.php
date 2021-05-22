3020391e58bc20006c622b80ec57b5ca6f8b6244
Juan Chavez <juan.chavezn1990@gmail.com>
2021-05-22 01:58:21 -0600

    building frontend shortcode

---
+++ Includes/frontend/shortcode.php
@@ -0,0 +1,184 @@
+<?php
+add_shortcode('HippoForm','hippoFormShortcode');
+function hippoFormShortcode(){
+    ob_start();
+    ?>
+    <section class="section is-style-wide column " id="hippo-main">
+        <form action="#" class="">
+            <div class="columns">
+                <div class="column field">
+                    <label for="" class="label">First Name*</label>
+                    <div class="control">
+                        <input type="text" id="firstName" class="input" placeholder="">
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">Middle Name</label>
+                    <div class="control">
+                        <input type="text" id="middleName" class="input" placeholder="">
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">Last Name*</label>
+                    <div class="control">
+                        <input type="text" id="lastName" class="input" placeholder="">
+                    </div>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column is-two-thirds field">
+                    <label for="" class="label">Street Address*</label>
+                    <div class="control">
+                        <input type="text" id="streetAddress" class="input" placeholder="">
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">Unit #</label>
+                    <div class="control">
+                        <input type="text" id="unitNumber" class="input" placeholder="">
+                    </div>
+                </div>
+            </div>
+            <div class="columns">
+            <div class="column field">
+                    <label for="" class="label">City*</label>
+                    <div class="control">
+                        <input type="text" id="city" class="input" placeholder="">
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">State*</label>
+                    <div class="control select">
+                        <select name="" id="state">
+                            <option value=""></option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky[D</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts[D</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania[D</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia[D</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+
+                        </select>
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">Zip Code*</label>
+                    <div class="control">
+                        <input type="text" id="zipCode" class="input" placeholder="">
+                    </div>
+                </div>
+            </div>
+            <div>
+            <div class="columns">
+                <div class="column field">
+                    <label for="" class="label">Date of Birth*</label>
+                    <div class="control">
+                        <input type="text" id="birthday" class="input" placeholder="MM/DD/YYYY">
+                    </div>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column field">
+                    <label for="" class="label">Phone Number*</label>
+                    <div class="control">
+                        <input type="text" id="phoneNumber" class="input" placeholder="">
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">Email Address*</label>
+                    <div class="control">
+                        <input type="text" id="email" class="input" placeholder="">
+                    </div>
+                </div>
+            </div>
+            <label for="" class="label">Is this a hose, condo or HO5?*</label>
+            <div class="columns hippo-radio is-spaced">
+                <div class="control column is-4">
+                    <label class="radio columns house">
+                        <div class="column hippo-image is-3"><img src="<?php echo plugin_dir_url( dirname( __DIR__ ),1).'Assets/img/House.jpg' ?>" alt=""></div>
+                        <div class="column"> 
+                            <span class="hippo-title">House</span>
+                            <span class="hippo-body">This may be a single-family home, townhose or duplex you own and live in.</span>
+                        </div>
+                        <div class="column is-1 hippo-radio-button"><input type="radio" name="type"></div>
+                    </label>
+                </div>
+                <div class="control column is-4">
+                    <label class="radio columns condo">
+                        <div class="column hippo-image is-3"><img src="<?php echo plugin_dir_url( dirname( __DIR__ ),1).'Assets/img/building.jpg' ?>" alt=""></div>
+                        <div class="column"> 
+                            <span class="hippo-title">Condo</span>
+                            <span class="hippo-body">This may be a single-family home, townhose or duplex you own and live in.</span>
+                        </div>
+                        <div class="column is-1 hippo-radio-button"><input type="radio" name="type"></div>
+                    </label>
+                </div>
+                <div class="control column is-4">
+                    <label class="radio columns ho5">
+                        <div class="column hippo-image is-3"><img src="<?php echo plugin_dir_url( dirname( __DIR__ ),1).'Assets/img/ho5.jpg' ?>" alt=""></div>
+                        <div class="column"> 
+                            <span class="hippo-title">HO5</span>
+                            <span class="hippo-body">This may be a single-family home, townhose or duplex you own and live in.</span>
+                        </div>
+                        <div class="column is-1 hippo-radio-button"><input type="radio" name="type"></div>
+                    </label>
+                </div>
+
+            </div>
+            <button class="button column is-success is-rounded is-4 is-offset-4">Submit</button>
+        </form>
+    </section>
+
+
+
+    <?php
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output ;
+}
+
+?>
--- hippo.php
+++ hippo.php
@@ -6,3 +6,12 @@ version: 1.0.0
 Author: Juan Chavez
 Author URI: https://juanchavez.dev 
 */
+
+function hippoLoadStyles(){
+    wp_enqueue_style('bulmaStyles',plugin_dir_url( __FILE__ ).'Assets/css/bulma.min.css','','0.9.2','all');
+    wp_enqueue_style('hippoStyles',plugin_dir_url( __FILE__ ).'Assets/css/hippo.css','','1.0.0','all');
+}
+add_action('wp_enqueue_scripts','hippoLoadStyles');
+
+//Include Section
+include_once 'Includes/frontend/shortcode.php';
